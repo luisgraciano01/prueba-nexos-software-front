2b449a2afdfe022057ad43b76165f2a2e6224303
Luis Graciano <luiiixxxofficial@gmail.com>
2021-08-08 14:51:06 -0500

    feat: agrega módulo login que tiene el compoente con el formulario de iniciar sesión

---
+++ src/app/features/login/login.component.ts
@@ -0,0 +1,70 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  TrackByFunction,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IUsuario } from '@core/interfaces/usuario';
+import { SessionService } from '@core/services/session.service';
+import { UsuarioService } from '@core/services/usuario.service';
+import { Subject } from 'rxjs';
+import { finalize, takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent implements OnInit {
+  public listaUsuarios: IUsuario[];
+  public loginForm: FormGroup;
+
+  private destroy$: Subject<boolean>;
+
+  constructor(
+    private usarioService: UsuarioService,
+    private sessionService: SessionService,
+    private formBuilder: FormBuilder,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+    this.listaUsuarios = [];
+    this.destroy$ = new Subject<boolean>();
+    this.loginForm = this.formBuilder.group({
+      usuario: [null, [Validators.required]],
+    });
+  }
+
+  get usuarioFormControl() {
+    return this.loginForm.controls.usuario;
+  }
+
+  ngOnInit(): void {
+    this.usarioService
+      .findAll()
+      .pipe(finalize(() => this.changeDetectorRef.detectChanges()))
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response) => {
+        if (response.exitoso && response.data) {
+          this.listaUsuarios = response.data;
+        }
+      });
+  }
+
+  trackByFnUsuario: TrackByFunction<IUsuario> = (
+    _index: number,
+    usuario: IUsuario
+  ) => usuario.idUsuario;
+
+  login() {
+    if (this.loginForm.valid) {
+      this.sessionService.setUsuario(
+        this.listaUsuarios.find(
+          (usuario) => usuario.idUsuario == this.usuarioFormControl.value
+        )
+      );
+    }
+  }
+}
